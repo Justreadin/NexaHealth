813e78e2e2d7ad25e55cc6d89ef8e3cdec70bd9a
Justreadin <dave.400g@gmail.com>
2025-07-26 07:20:11 +0100

    nexahealth updated mvp

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
         "http://127.0.0.1:*",
         "http://localhost:5501",
         "http://127.0.0.1:5501",
-        "https://nexahealth.vercel.app"
+        "https://nexahealth.vercel.app",
+        "https://nexahealth.life"
     ],
     allow_credentials=True,
     allow_methods=["*"],
