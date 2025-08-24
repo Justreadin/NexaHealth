fb297c7e053188d7d093802a6a0dcca1a9d358aa
Justreadin <dave.400g@gmail.com>
2025-08-24 16:36:07 +0100

    I changed verification logic

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,22 +1,22 @@
 // frontend/firebase.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { 
   getAnalytics, 
   logEvent,
   setUserProperties,
   setUserId
-} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+ 
 const firebaseConfig = {
     apiKey: "AIzaSyDz4vI2Vfm6oYDsU0-UL0G-GO-emT8y0QM",
     authDomain: "nexa-health.firebaseapp.com",
     projectId: "nexa-health",
     storageBucket: "nexa-health.firebasestorage.app",
     messagingSenderId: "616215979591",
-    appId: "1:616215979591:web:6bcf62880219a2e9a83362"
+    appId: "1:616215979591:web:6bcf62880219a2e9a83362",
+    measurementId: "G-34ST9GPLFL"
   };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -24,7 +24,7 @@ const analytics = getAnalytics(app);
 // Track a page view
 export function trackPage(pageName) {
   logEvent(analytics, "page_view", { 
-    page_name: pageName,
+    page_title: pageName,
     page_location: window.location.href
   });
 }
@@ -62,11 +62,10 @@ export function trackButtonClick(buttonName, location) {
 // Track user engagement
 export function trackEngagement(timeEngaged, engagementType) {
   logEvent(analytics, "user_engagement", {
-    engagement_time: timeEngaged,
+    engagement_time_msec: timeEngaged,
     engagement_type: engagementType
   });
 }
-
 // Set user properties (if you have user authentication)
 export function setUserAnalyticsProperties(properties) {
   setUserProperties(analytics, properties);
