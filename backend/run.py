40e162be100d8a7509106317910ef579c67404e3
Justreadin <dave.400g@gmail.com>
2025-08-30 10:10:16 +0100

    I changed verification logic

--- backend/run.py
+++ backend/run.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)  # no reload
